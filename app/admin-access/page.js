9e6ee088a987da237f50db50ac29130aaa2b69ac
Abhinay Singh <abhinay.anarayn@dbalounge.com>
2024-05-15 14:08:18 +0530

    page route updated

--- app/admin-access/page.js
+++ app/admin-access/page.js
@@ -1,13 +1,11 @@
 import SearchInput from '@/components/core/Input/SearchInput'
 import React from 'react'
-import Table from '@/components/core/Input/Table'
 
 function AdminDashboardPage() {
     return (
         <div>
             <SearchInput />
             <hr />
-            <Table header={["Metric", "Unit | Denomination | Currency", "Year", "Month/Quater", "Value", "Action"]} ></Table>
         </div>
     )
 }
--- utils/pageRoutes.js
+++ utils/pageRoutes.js
@@ -1,6 +1,6 @@
 const pageRoutes = {
     ADMIN_DASHBOARD_PAGE: () => `/admin-access`,
-    DASHBOARD_PAGE: () => `/dashboard`,
+    DASHBOARD_PAGE: () => `/user-access`,
     FORGOT_PASSWORD: () => `/forgot-password`,
     SIGIN_PAGE: () => `/`
 
