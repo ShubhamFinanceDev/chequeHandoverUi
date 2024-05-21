5241240f53f82e01dca2ca85512d00b67cfa67da
Abhinay Singh <abhinay.anarayn@dbalounge.com>
2024-05-21 14:50:23 +0530

    footer fixed

--- components/core/Footer.js
+++ components/core/Footer.js
@@ -3,9 +3,12 @@ import React from 'react'
 const Footer = () => {
     const date = new Date()
     return (
-        <footer>
-            <p>Copyright © 2018-{date.getFullYear()} | Shubham Housing Development Finance Company Limited | All Rights Reserved</p>
-        </footer>
+        <>
+            <span className="footer-span" />
+            <footer>
+                <p>Copyright © 2018-{date.getFullYear()} | Shubham Housing Development Finance Company Limited | All Rights Reserved</p>
+            </footer>
+        </>
     )
 }
 
