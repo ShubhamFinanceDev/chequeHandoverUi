2f3548c88de3c87ed18572cb40f3ea9668e67a63
Abhinay Singh <abhinay.anarayn@dbalounge.com>
2024-05-21 15:07:14 +0530

    error auto hide added

--- components/core/ValidationMsg.js
+++ components/core/ValidationMsg.js
@@ -1,10 +1,17 @@
 "use client"
 
-import React from 'react'
+import useActionDispatch from '@/hooks/useActionDispatch'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 const ValidationMsg = () => {
     const { type, msg } = useSelector(state => state.validationSlice)
+    const { resetValidation } = useActionDispatch()
+    useEffect(() => {
+        setTimeout(() => {
+            resetValidation()
+        }, 1000);
+    }, [type, msg])
     return (msg && <p className={`text-center ${type}`}>{msg}</p>)
 }
 
