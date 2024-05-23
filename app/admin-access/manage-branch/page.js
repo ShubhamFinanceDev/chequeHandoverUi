839040aaeee025f00c8f655b6eb189ce288e9f1d
Abhinay Singh <manish.singh@dbalounge.com>
2024-05-23 14:26:04 +0530

    branchlist show

--- app/admin-access/manage-branch/page.js
+++ app/admin-access/manage-branch/page.js
@@ -1,12 +1,34 @@
+'use client'
 import BranchExcelUploadForm from '@/components/page/manage-branch/BranchExcelUploadForm'
-import React from 'react'
 import Table from '@/components/core/Input/Table'
+import useFetchDataHooks from '@/hooks/useFetchDataHooks'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
 const BranchManagePage = () => {
+    const { branchList } = useSelector((state) => state.globalSlice)
+
+
+    const { fetchBranchList } = useFetchDataHooks()
+
+    useEffect(() => {
+        fetchBranchList()
+    }, [])
+
     return (
         <div>
             <BranchExcelUploadForm />
-            <Table header={["BranchName", "state"]} className='mt-2' >
+            <Table header={["S/N", "BranchName", "state"]} className='mt-2' >
+                {branchList?.map((m, index) => {
+                    return (
+                        <tr key={`branchdata__` + m.id}>
+                            <td>{index + 1}</td>
+                            <td>{m.name}</td>
+                            <td>{m.state}</td>
+                        </tr>
+                    )
+                })}
+
             </Table>
         </div>
     )
