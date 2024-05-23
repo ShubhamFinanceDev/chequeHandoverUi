2d81dfcb83de83e6c4faf0fa352ba42e08e8427e
Abhinay Singh <manish.singh@dbalounge.com>
2024-05-23 12:26:03 +0530

    branch table add

--- app/admin-access/manage-branch/page.js
+++ app/admin-access/manage-branch/page.js
@@ -1,10 +1,13 @@
 import BranchExcelUploadForm from '@/components/page/manage-branch/BranchExcelUploadForm'
 import React from 'react'
+import Table from '@/components/core/Input/Table'
 
 const BranchManagePage = () => {
     return (
         <div>
             <BranchExcelUploadForm />
+            <Table header={["BranchName", "state"]} className='mt-2' >
+            </Table>
         </div>
     )
 }
