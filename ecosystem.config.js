9e272edaf70de07f3fdd3aa565380d596390d92e
nainish <pmo_dbalounge@shubham.co>
2024-05-30 14:31:19 +0530

    pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,32 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  apps: [
+
+    {
+
+      name: 'next-app',
+
+      script: 'node_modules/next/dist/bin/next',
+
+      args: 'start',
+
+      instances: 1,
+
+      autorestart: true,
+
+      watch: false,
+
+      max_memory_restart: '4G',
+
+      env: {
+
+        NODE_ENV: 'production',
+
+        PORT: 3000
+
+      },
+
+    },
+
+  ],
 };
